05024e2352d98d9e3ddcceded891e4532b16cd17
Jim Fowler <Jim.Fowler@sas.com>
2017-03-30 10:47:56 -0400

    add config server

---
+++ config_server/src/main/java/com/github/jofowler/config/App.java
@@ -0,0 +1,13 @@
+package com.github.jofowler.config;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
